10f6837439a2f0ad4f885edc3e857c493811f41d
mrs1669 <mrs1669.develop@gmail.com>
2020-04-07 00:30:25 +0900

    make single App

---
+++ AccountSim/AccountSim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccountSim
+//
+//  Created by Takumi Muraishi on 2020/04/07.
+//  Copyright © 2020 Takumi Muraishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
