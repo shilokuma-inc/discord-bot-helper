532ff0764c0c22c321613b2310cfe4523811e2a4
yut0124 <b1018133@fun.ac.jp>
2020-04-08 07:22:41 +0900

    最初の画面からの画面遷移を実装

---
+++ AccountSim/AccountSim/AccountDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  AccountDataClass.swift
+//  AccountSim
+//
+//  Created by Yutaro Sakai on 2020/04/07.
+//  Copyright © 2020 Takumi Muraishi. All rights reserved.
+//
+
+import Foundation
+
+public class AccountData {
+    static var 練習モード: Int = 0
+}
--- AccountSim/AccountSim/ViewController.swift
+++ AccountSim/AccountSim/ViewController.swift
@@ -8,11 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public var 練習モード: Int = 0
 
+class ViewController: UIViewController {
+    let homeViewController = UIStoryboard(name: "HomeViewController", bundle: nil).instantiateInitialViewController()
+    
+    @IBOutlet weak var 新規登録の練習ボタン: UIButton!
+    @IBOutlet weak var ログインの練習ボタン: UIButton!
+    @IBOutlet weak var パスワードを忘れた時の練習ボタン: UIButton!
+    @IBOutlet weak var 初めから自分で練習ボタン: UIButton!
+    
+    @IBAction func 新規登録の練習関数(_ sender: Any) {
+        練習モード = 1
+        print(練習モード)
+        
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.reveal
+        transition.subtype = CATransitionSubtype.fromTop
+        view.window!.layer.add(transition, forKey: kCATransition)
+        homeViewController?.modalPresentationStyle = .fullScreen
+        self.present(homeViewController!, animated: false)
+    }
+    @IBAction func ログインの練習関数(_ sender: Any) {
+        練習モード = 2
+        print(練習モード)
+        
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.reveal
+        transition.subtype = CATransitionSubtype.fromTop
+        view.window!.layer.add(transition, forKey: kCATransition)
+        homeViewController?.modalPresentationStyle = .fullScreen
+        self.present(homeViewController!, animated: false)
+    }
+    @IBAction func パスワードを忘れた時の練習関数(_ sender: Any) {
+        練習モード = 3
+        print(練習モード)
+        
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.reveal
+        transition.subtype = CATransitionSubtype.fromTop
+        view.window!.layer.add(transition, forKey: kCATransition)
+        homeViewController?.modalPresentationStyle = .fullScreen
+        self.present(homeViewController!, animated: false)
+    }
+    @IBAction func 初めから自分で練習関数(_ sender: Any) {
+        練習モード = 4
+        print(練習モード)
+        
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.reveal
+        transition.subtype = CATransitionSubtype.fromTop
+        view.window!.layer.add(transition, forKey: kCATransition)
+        homeViewController?.modalPresentationStyle = .fullScreen
+        self.present(homeViewController!, animated: false)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
