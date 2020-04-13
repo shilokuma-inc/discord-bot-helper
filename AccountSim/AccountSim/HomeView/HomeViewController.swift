ba5c30f1c00d8bd120a8f285cd355b132404783a
yut0124 <b1018133@fun.ac.jp>
2020-04-13 16:38:57 +0900

    HomeViewにアプリアイコンボタンを配置

--- AccountSim/AccountSim/HomeView/HomeViewController.swift
+++ AccountSim/AccountSim/HomeView/HomeViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-    override func viewDidLoad() {
+class HomeViewController: UIViewController{
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         
     }
