f03d1f721c8b6ed8702c31fa90a83a9581cfc60e
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-17 14:07:46 +0900

    make discord-bot-launcher

---
+++ discord-bot-launcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  discord-bot-launcher
+//
+//  Created by 村石 拓海 on 2024/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ discord-bot-launcher/discord_bot_launcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  discord_bot_launcherApp.swift
+//  discord-bot-launcher
+//
+//  Created by 村石 拓海 on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct discord_bot_launcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
