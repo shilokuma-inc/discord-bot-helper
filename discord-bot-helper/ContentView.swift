3095ab897a5dbf9a2cb44937447e0bf867d190ce
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-19 03:22:09 +0900

    add sample post method

--- discord-bot-helper/ContentView.swift
+++ discord-bot-helper/ContentView.swift
@@ -6,20 +6,55 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ContentView: View {
+    
+    @State var inputName = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            TextField("URLを入れてください", text: $inputName)
             Button(action: {
+                postDiscordWebhook(url: inputName)
             }, label: {
                 Text("Send")
             })
         }
         .padding()
+    }
+}
+
+private func postDiscordWebhook(url: String) {
+    let baseUrlString = url
+    let param: Parameters = [
+            "content": "Hello"
+        ]
+    let headers: HTTPHeaders = ["Content-Type": "application/json"]
+
+    
+    var request = URLRequest(url: (URL(string: baseUrlString) ?? URL(string: "https://www.apple.com/")!))
+    request.httpMethod = HTTPMethod.post.rawValue
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = NSString(
+        data: try! JSONSerialization.data(withJSONObject: param as Any, 
+                                          options:JSONSerialization.WritingOptions.prettyPrinted
+                                         ),
+        encoding: String.Encoding.utf8.rawValue
+    )!
+        .data(using: String.Encoding.utf8.rawValue)
+    AF.request(request)
+    .responseData { response in
+        switch response.result {
+            case .success(let data):
+                print("success")
+            case .failure(let error):
+                print("error")
+        }
     }
 }
 
