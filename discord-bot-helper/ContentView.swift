b3f621f4c334d12a1e760961d3c74829437cb225
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-19 00:36:18 +0900

    replace discord-bot-helper

--- discord-bot-helper/ContentView.swift
+++ discord-bot-helper/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  discord-bot-launcher
+//  discord-bot-helper
 //
 //  Created by 村石 拓海 on 2024/04/17.
 //
--- discord-bot-helper/discord_bot_launcherApp.swift
+++ discord-bot-helper/discord_bot_launcherApp.swift
@@ -1,6 +1,6 @@
 //
 //  discord_bot_launcherApp.swift
-//  discord-bot-launcher
+//  discord-bot-helper
 //
 //  Created by 村石 拓海 on 2024/04/17.
 //
