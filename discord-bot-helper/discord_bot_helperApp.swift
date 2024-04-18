0da3b222d1414c5344b7cd7c675fa4f864ce644c
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-19 00:37:16 +0900

    replace discord_bot_helper

--- discord-bot-helper/discord_bot_helperApp.swift
+++ discord-bot-helper/discord_bot_helperApp.swift
@@ -1,5 +1,5 @@
 //
-//  discord_bot_launcherApp.swift
+//  discord_bot_helperApp.swift
 //  discord-bot-helper
 //
 //  Created by 村石 拓海 on 2024/04/17.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct discord_bot_launcherApp: App {
+struct discord_bot_helperApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
