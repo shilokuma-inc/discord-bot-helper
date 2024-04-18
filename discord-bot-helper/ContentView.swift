54ef3870b744bc0313c16e5244d36cc83353ac45
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-19 02:42:59 +0900

    add send button

--- discord-bot-helper/ContentView.swift
+++ discord-bot-helper/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button(action: {
+            }, label: {
+                Text("Send")
+            })
         }
         .padding()
     }
