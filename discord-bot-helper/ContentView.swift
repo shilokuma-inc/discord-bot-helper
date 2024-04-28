51a125b3774ce63751d5c5f8ac9203a16b758aa0
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-28 14:59:55 +0900

    [feat] adjust component space

--- discord-bot-helper/ContentView.swift
+++ discord-bot-helper/ContentView.swift
@@ -14,11 +14,15 @@ struct ContentView: View {
     private var viewModel = SendMessageViewModel()
     
     var body: some View {
-        VStack {
+        VStack(spacing: .zero) {
             TextField("URLを入れてください", text: $inputURL)
                 .textFieldStyle(.roundedBorder)
+            Spacer()
+                .frame(height: 8.0)
             TextField("メッセージを入れてください", text: $inputContext)
                 .textFieldStyle(.roundedBorder)
+            Spacer()
+                .frame(height: 16.0)
             Button(action: {
                 viewModel.postDiscordWebhook(url: inputURL,
                                              messageEntity: MessageEntity(
