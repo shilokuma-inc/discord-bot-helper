b560a039c58e658bc967713f58c82d218b8a9a86
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-28 15:00:31 +0900

    [chore] style

--- discord-bot-helper/ContentView.swift
+++ discord-bot-helper/ContentView.swift
@@ -17,12 +17,16 @@ struct ContentView: View {
         VStack(spacing: .zero) {
             TextField("URLを入れてください", text: $inputURL)
                 .textFieldStyle(.roundedBorder)
+            
             Spacer()
                 .frame(height: 8.0)
+            
             TextField("メッセージを入れてください", text: $inputContext)
                 .textFieldStyle(.roundedBorder)
+            
             Spacer()
                 .frame(height: 16.0)
+            
             Button(action: {
                 viewModel.postDiscordWebhook(url: inputURL,
                                              messageEntity: MessageEntity(
