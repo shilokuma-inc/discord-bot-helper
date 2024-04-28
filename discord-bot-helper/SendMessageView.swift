ab8711ae0762f0f7a7fcf858673e8b8010634c1d
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-28 15:23:34 +0900

    [feat] add background

--- discord-bot-helper/SendMessageView.swift
+++ discord-bot-helper/SendMessageView.swift
@@ -14,30 +14,44 @@ struct SendMessageView: View {
     private var viewModel = SendMessageViewModel()
     
     var body: some View {
-        VStack(spacing: .zero) {
-            TextField("URLを入れてください", text: $inputURL)
-                .textFieldStyle(.roundedBorder)
-            
-            Spacer()
-                .frame(height: 8.0)
-            
-            TextField("メッセージを入れてください", text: $inputContext)
-                .textFieldStyle(.roundedBorder)
-            
-            Spacer()
-                .frame(height: 16.0)
-            
-            Button(action: {
-                viewModel.postDiscordWebhook(url: inputURL,
-                                             messageEntity: MessageEntity(
-                                                content: inputContext
-                                             )
-                )
-            }, label: {
-                Text("Send")
-            })
+        ZStack {
+            Color.cyan
+                .opacity(0.2)
+                .ignoresSafeArea()
+            VStack(spacing: .zero) {
+                TextField("URLを入れてください", text: $inputURL)
+                    .textFieldStyle(.roundedBorder)
+                
+                Spacer()
+                    .frame(height: 8.0)
+                
+                TextField("メッセージを入れてください", text: $inputContext)
+                    .textFieldStyle(.roundedBorder)
+                    .lineLimit(0...10)
+                
+                Spacer()
+                    .frame(height: 16.0)
+                
+                Button(action: {
+                    viewModel.postDiscordWebhook(url: inputURL,
+                                                 messageEntity: MessageEntity(
+                                                    content: inputContext
+                                                 )
+                    )
+                }, label: {
+                    Text("メッセージを送信！")
+                        .font(.system(size: 24, weight: .semibold, design: .default))
+                        .foregroundStyle(.gray)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 30.0)
+                                .foregroundStyle(.ultraThickMaterial)
+                                .shadow(radius: 5.0)
+                        )
+                })
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
