1bd99319f045a7b3be6a120cc8adbdf6569a832e
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-01 07:18:45 +0900

    [fix] add brew install swiftlint

--- discord-bot-helper/ci_scripts/ci_post_clone.sh
+++ discord-bot-helper/ci_scripts/ci_post_clone.sh
@@ -6,5 +6,6 @@
 #  Created by 村石 拓海 on 2024/05/01.
 #  
 
+brew install swiftlint
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
