28c308e63443c0b492600902ddc58196f6dce97f
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-30 21:12:49 +0900

    [feat] make SettingView

---
+++ discord-bot-helper/SettingView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingView.swift
+//  discord-bot-helper
+//
+//  Created by 村石 拓海 on 2024/04/30.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("設定画面")
+    }
+}
