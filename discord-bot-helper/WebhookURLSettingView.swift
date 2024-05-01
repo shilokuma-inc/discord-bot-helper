85ae43c6442afb902fff5965666253cab811cca2
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-01 09:41:00 +0900

    [feat] add Button for dismiss

--- discord-bot-helper/WebhookURLSettingView.swift
+++ discord-bot-helper/WebhookURLSettingView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct WebhookURLSettingView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         ZStack {
             Color.pink
                 .opacity(0.2)
                 .ignoresSafeArea(edges: [.top])
-            Text("URL設定")
+            VStack {
+                Text("URL設定")
+                Button(action: {
+                    dismiss()
+                }) {
+                    Text("設定完了")
+                }
+            }
         }
     }
 }
