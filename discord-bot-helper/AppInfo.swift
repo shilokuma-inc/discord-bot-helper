c68a5c2c86bf50397c801293624228263cac0417
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-05 08:18:22 +0900

    [feat] make AppInfo.swift

---
+++ discord-bot-helper/AppInfo.swift
@@ -0,0 +1,15 @@
+//
+//  AppInfo.swift
+//  discord-bot-helper
+//
+//  Created by 村石 拓海 on 2024/05/05.
+//
+
+import Foundation
+
+final class AppInfo {
+    func getVersion() -> String {
+        guard let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else { return "" }
+        return version
+    }
+}
