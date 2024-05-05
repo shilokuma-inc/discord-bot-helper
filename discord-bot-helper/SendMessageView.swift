92baffbe50fdbe284ecb4fd72f1dea5dd0570557
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-06 05:05:05 +0900

    [feat] make onChange extension

--- discord-bot-helper/SendMessageView.swift
+++ discord-bot-helper/SendMessageView.swift
@@ -32,7 +32,7 @@ struct SendMessageView: View {
                             placeholder: "URLを入れてください",
                             text: $inputURL
                         )
-                        .onChange(of: inputURL) {
+                        .onChange(of: inputURL, initial: true) { _ in
                             textFieldValidation()
                         }
                         
@@ -132,6 +132,21 @@ extension SendMessageView {
             isTapEnable = true
         }
     }
+}
+
+extension View {
+  @ViewBuilder func onChange<V: Equatable>(of value: V, initial: Bool, perform action: @escaping (_ newValue: V) -> Void) -> some View {
+    if #available(iOS 17.0, *) {
+      onChange(of: value, initial: initial) {
+        action($1)
+      }
+    } else if initial {
+      onAppear { action(value) }
+        .onChange(of: value, perform: action)
+    } else {
+      onChange(of: value, perform: action)
+    }
+  }
 }
 
 struct SendMessageView_Previews: PreviewProvider {
