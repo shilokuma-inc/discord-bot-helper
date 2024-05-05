f2333b5bea71869e3523131802b87534cfa2a416
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-06 05:44:43 +0900

    [feat] make class BannerViewController

--- discord-bot-helper/BannerViewController.swift
+++ discord-bot-helper/BannerViewController.swift
@@ -5,4 +5,26 @@
 //  Created by 村石 拓海 on 2024/05/06.
 //
 
-import Foundation
+import UIKit
+
+protocol BannerViewControllerWidthDelegate: AnyObject {
+    func bannerViewController(_ bannerViewController: BannerViewController, didUpdate width: CGFloat)
+}
+
+class BannerViewController: UIViewController {
+    weak var delegate: BannerViewControllerWidthDelegate?
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        delegate?.bannerViewController(self, didUpdate: view.frame.inset(by: view.safeAreaInsets).size.width)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator){
+        coordinator.animate { _ in
+            // do nothing
+        } completion: { _ in
+            self.delegate?.bannerViewController(self, didUpdate: self.view.frame.inset(by: self.view.safeAreaInsets).size.width)
+        }
+    }
+}
