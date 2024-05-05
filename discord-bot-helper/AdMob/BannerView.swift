a7e00cf89f0f3923d3bdcc096b9f5a528dc087c7
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-06 05:50:31 +0900

    [feat] make BannerView

--- discord-bot-helper/AdMob/BannerView.swift
+++ discord-bot-helper/AdMob/BannerView.swift
@@ -5,4 +5,47 @@
 //  Created by 村石 拓海 on 2024/05/06.
 //
 
-import Foundation
+import SwiftUI
+import GoogleMobileAds
+
+struct BannerView: UIViewControllerRepresentable {
+    @State private var viewWidth: CGFloat = .zero
+    private let bannerView = GADBannerView()
+    private let adUnitID = "ca-app-pub-7683522872696760~1123417271"
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let bannerViewController = BannerViewController()
+        bannerView.adUnitID = adUnitID
+        bannerView.rootViewController = bannerViewController
+        bannerViewController.view.addSubview(bannerView)
+        bannerViewController.delegate = context.coordinator
+
+        return bannerViewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        guard viewWidth != .zero else { return }
+
+        // Request a banner ad with the updated viewWidth.
+        bannerView.adSize = GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(viewWidth)
+        bannerView.load(GADRequest())
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, BannerViewControllerWidthDelegate {
+        let parent: BannerView
+
+        init(_ parent: BannerView) {
+            self.parent = parent
+        }
+
+        // MARK: - BannerViewControllerWidthDelegate methods
+        func bannerViewController(_ bannerViewController: BannerViewController, didUpdate width: CGFloat) {
+
+            parent.viewWidth = width
+        }
+    }
+}
