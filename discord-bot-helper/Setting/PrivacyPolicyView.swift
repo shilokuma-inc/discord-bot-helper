43f6f9d3d94874b35c1e84af738c7d34ee4ee3e7
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-05 09:03:15 +0900

    [feat] make PrivacyPolicyWebView

--- discord-bot-helper/Setting/PrivacyPolicyView.swift
+++ discord-bot-helper/Setting/PrivacyPolicyView.swift
@@ -5,4 +5,30 @@
 //  Created by 村石 拓海 on 2024/05/05.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct PrivacyPolicyView: View {
+    let privacyPolicyUrlString: String = "https://shilokuma-inc.github.io/iOS-Release-Sample/PrivacyPolicy/discord-bot-helper/PrivacyPolicy.html"
+    
+    var body: some View {
+        if let privacyPolicyUrl = URL(string: privacyPolicyUrlString) {
+            PrivacyPolicyWebView(url: privacyPolicyUrl)
+        } else {
+            EmptyView()
+        }
+    }
+}
+
+struct PrivacyPolicyWebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
