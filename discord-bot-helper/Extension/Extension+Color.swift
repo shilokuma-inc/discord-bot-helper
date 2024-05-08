b472bee909ee70bf55bb0ca2a5a026515b38c8bc
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-08 21:11:34 +0900

    [feat] add 2gray color

--- discord-bot-helper/Extension/Extension+Color.swift
+++ discord-bot-helper/Extension/Extension+Color.swift
@@ -10,4 +10,6 @@ import SwiftUI
 extension Color {
     static var discordPurple = Color(red: 69 / 255, green: 73 / 255, blue: 239 / 255)
     static var discordGray = Color(red: 37 / 255, green: 39 / 255, blue: 43 / 255)
+    static var discordLightGray = Color(red: 42 / 255, green: 44 / 255, blue: 49 / 255)
+    static var discordSuperLightGray = Color(red: 89 / 255, green: 92 / 255, blue: 101 / 255)
 }
