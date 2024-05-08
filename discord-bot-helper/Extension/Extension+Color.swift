cfacff187438b3178b68ad5dd1720e6533108885
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-08 20:59:38 +0900

    [feat] make Extension+Color

--- discord-bot-helper/Extension/Extension+Color.swift
+++ discord-bot-helper/Extension/Extension+Color.swift
@@ -5,4 +5,9 @@
 //  Created by 村石 拓海 on 2024/05/08.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static var discordPurple = Color(red: 69 / 255, green: 73 / 255, blue: 239 / 255)
+    static var discordGray = Color(red: 37 / 255, green: 39 / 255, blue: 43 / 255)
+}
