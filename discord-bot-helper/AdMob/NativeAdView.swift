6ff53eccfbdf4737942b65b7d3df500c9888561d
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-09 20:45:12 +0900

    [feat] make NativeAdView

---
+++ discord-bot-helper/AdMob/NativeAdView.swift
@@ -0,0 +1,45 @@
+//
+//  NativeAdView.swift
+//  discord-bot-helper
+//
+//  Created by 村石 拓海 on 2024/05/09.
+//
+
+import SwiftUI
+import GoogleMobileAds
+
+struct NativeAdView: UIViewRepresentable {
+    let nativeAd: GADNativeAd
+    func makeUIView(context: Context) -> GADNativeAdView {
+        // swiftlint:disable:next force_cast
+        let nativeAdView: GADNativeAdView = Bundle.main.loadNibNamed("NativeAdView", owner: nil, options: nil)?.first as! GADNativeAdView
+
+        (nativeAdView.bodyView as? UILabel)?.text = nativeAd.body
+        nativeAdView.bodyView?.isHidden = nativeAd.body == nil
+
+        (nativeAdView.callToActionView as? UIButton)?.setTitle(nativeAd.callToAction, for: .normal)
+        nativeAdView.callToActionView?.isHidden = nativeAd.callToAction == nil
+
+        (nativeAdView.iconView as? UIImageView)?.image = nativeAd.icon?.image
+        nativeAdView.iconView?.isHidden = nativeAd.icon == nil
+
+        (nativeAdView.storeView as? UILabel)?.text = nativeAd.store
+        nativeAdView.storeView?.isHidden = nativeAd.store == nil
+
+        (nativeAdView.priceView as? UILabel)?.text = nativeAd.price
+        nativeAdView.priceView?.isHidden = nativeAd.price == nil
+
+        (nativeAdView.advertiserView as? UILabel)?.text = nativeAd.advertiser
+        nativeAdView.advertiserView?.isHidden = nativeAd.advertiser == nil
+
+        nativeAdView.callToActionView?.isUserInteractionEnabled = false
+
+        nativeAdView.nativeAd = nativeAd
+        
+        return nativeAdView
+    }
+    
+    func updateUIView(_ uiView: GADNativeAdView, context: Context) {
+        
+    }
+}
